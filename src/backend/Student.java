87f51599a913533f5c1bb5fae25edd501b53c96c
AbdelrahmanAtef5172 <abdelrahmanatef426@gmail.com>
2025-10-24 19:57:42 +0300

    Update Student.java
    - creating class setters and getters

--- src/backend/Student.java
+++ src/backend/Student.java
@@ -9,6 +9,44 @@ public class Student {
     private String department;
     private float GPA ;
 
+    //GETTERS FOR ALL ATTRIBUTES
+    public int getId() {return id;}
+    public String getFullName() {return fullName;}
+    public int getAge() { return age; }
+    public String getGender() {return gender;}
+    public String getDepartment() { return department;}
+    public float getGPA() { return GPA; }
+
+    //SETTERS FOR ALL ATTRIBUTES
+    public void setGPA(float GPA) {
+        if(GPA < 0 || GPA > 4) {
+            throw new IllegalArgumentException("INVALID GPA IS ENTERED");
+        }else {this.GPA = GPA;}}
+    public void setGPA(){
+        //logic to generate id automatically
+        }
+    public void setAge(int age) {
+        if(age<4 || age>27){
+            throw new IllegalArgumentException("INVALID AGE IS ENTERED ");
+        }else{this.age=age;}}
+    public void setId(int id) {
+        if(id<1){
+            throw new IllegalArgumentException("INVALID ID IS ENTERED");
+        }else{this.id=id;}}
+    public void setFullName(String fullName) {
+        if(fullName.split(" ").length<2 || fullName.isEmpty()){
+            throw new IllegalArgumentException("THIS IS INVALID FULLNAME");}
+        for(int i = 0 ; i<fullName.length();i++ ){
+            if(!Character.isLetter(fullName.charAt(i))){
+                throw new IllegalArgumentException("INVALID NAME IS ENTERED");}}
+        this.fullName = fullName;}
+    public void setGender(String gender) {
+        gender = gender.strip().toLowerCase();
+        if(gender.equals("male")||gender.equals("female")){
+            throw new IllegalArgumentException("INVALID GENDER IS ENTERED");
+        }else{this.gender = gender;}}
+    public void setDepartment(String department) {
+        this.department = department.strip();}
 
 
 
@@ -28,18 +66,4 @@ public class Student {
         this.setAge(age);
         this.setGender(gender);
         this.setDepartment(department);}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 }
