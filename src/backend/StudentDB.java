0b5d54c4de7000ec3e4fd739cf9dd36962cd3cb8
AbdelrahmanAtef5172 <abdelrahmanatef426@gmail.com>
2025-10-24 22:51:42 +0300

    Create StudentDB.java
    - added class attributes
    - added class constructor
    - added method to read from file

---
+++ src/backend/StudentDB.java
@@ -0,0 +1,35 @@
+package Frontend.src.backend;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class StudentDB {
+
+
+    //CLASS ATTRIBUTES
+    private String fileName;
+    private ArrayList<Student> students ;
+
+    //CLASS CONSTRUCTOR
+    public StudentDB (String fileName){
+        this.fileName = fileName.strip();
+        this.students = new ArrayList<>();}
+
+    //METHOD TO READ FROM THE FILE
+    public void readFromFile() throws FileNotFoundException {
+        File file  = new File(this.fileName);
+        Scanner fileReader = new Scanner(file);
+        while(fileReader.hasNextLine()){
+            String record = fileReader.nextLine().strip();
+            String[] recordArray = record.split(",");
+            int id  = Integer.parseInt(recordArray[0].strip());
+            String fullName = recordArray[1].strip();
+            int age = Integer.parseInt(recordArray[2].strip());
+            String gender = recordArray[3].strip().toLowerCase();
+            String department = recordArray[4].strip();
+            float gpa = Float.parseFloat(recordArray[5].strip());
+            this.students.add(new Student(id,fullName,age,gender,department,gpa));}}
+
+
+}
