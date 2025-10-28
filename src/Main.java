e8caadb71a3cf88251ba7720d939ebd30c3fbcec
tango12x <galaltaman123@gmail.com>
2025-10-28 11:41:40 +0300

    Main class created

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import gui.*;
+import backend.*;
+
+
+public class Main {
+    public static void main(String[] args) {
+        // Initialize backend components
+        login frame = new login();
+        frame.setVisible(true);
+        frame.setLocationRelativeTo(null);
+    }
+}
